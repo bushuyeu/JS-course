ceb77b2a64d626ead26680c029506f6a82a3891f
Pavel Bushuyeu <pavelbushuyeu@gmail.com>
2023-12-30 20:30:52 -0500

    variables and values

---
+++ 01-Fundamentals-Part-1/Values and Variables/script.js
@@ -0,0 +1,41 @@
+let firstName = "Jonas"
+//firstName - Variable 
+//Jonas - Value
+console.log(firstName);
+
+// conventions and rules
+// camelCase
+let first = "matilda";
+let lastName = "Smith"
+
+let 3years = 3; // SyntaxError to name the variable 3years, vars cannot start with numbers
+
+//vars cannot use JS reserved words
+let new = 27; 
+let function = 88;
+let name = "Jonas" //reserved, but allowed 
+
+
+// vars can only include:
+// numbers
+// letters
+// _
+// $
+
+// vars should start with a lowercase, uppercase is reserved for objects
+
+// vars that are all in uppercase are reserved for constants that we know will never change
+
+let PI = 3.1415;
+
+// make sure vars are descriptive
+
+//e.g.:
+let myFirstJob = "Programmer";
+let myCurrentJob = "Teacher";
+
+//ant not:
+
+let job1 = "Programmer";
+let job2 = "teacher";
+
