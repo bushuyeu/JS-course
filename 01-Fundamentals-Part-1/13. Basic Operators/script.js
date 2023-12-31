5aa0ee0b916bd1dcbfb3455e6390a1a14c196c64
Pavel Bushuyeu <pavelbushuyeu@gmail.com>
2023-12-31 00:19:35 -0500

    operator precedence

---
+++ 01-Fundamentals-Part-1/13. Basic Operators/script.js
@@ -0,0 +1,35 @@
+// Math operators
+
+const currentYear = 2023;
+const birthYear = 1991;
+const agePavel = currentYear - birthYear;
+
+console.log(agePavel);
+
+const eight = 2 ** 3; // 2 to the power of 8
+console.log(eight);
+
+const firstName = 'Pavel';
+const lastName = 'Bushuyeu';
+const fullName = firstName + ' ' + lastName;
+
+console.log(fullName);
+
+
+// type of operator
+
+// assignment operator
+
+let x = 10 + 5; //15  
+console.log(x);
+
+x += 10; // x = x + 10 = 15 + 10 = 25
+console.log(x);
+x *= 2;
+console.log(x);
+x++;
+console.log(x);
+x--;
+console.log(x);
+
+console.log(fullName.length >= 0);
