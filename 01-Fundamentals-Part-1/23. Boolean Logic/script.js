1793c4a2bde642eb13199f0428c3c49a6a64dd5d
Pavel Bushuyeu <pavelbushuyeu@gmail.com>
2024-01-02 01:06:55 -0500

    boolean operators

---
+++ 01-Fundamentals-Part-1/23. Boolean Logic/script.js
@@ -0,0 +1,12 @@
+// AND — both vars A and B should be TRUE to return true
+
+// OR - either var A or B should be TRUE to return true
+
+// NOT - inverts boolean value
+
+var age = 18;
+var lowerLimit = 16;
+var upperLimit = 30;
+
+console.log(age > lowerLimit AND age < upperLimit);
+console.log(!(age > lowerLimit)); //fasle
