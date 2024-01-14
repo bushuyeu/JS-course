9be4c1efa6937c46e1ff00aa995e309c451df644
Pavel Bushuyeu <pavelbushuyeu@gmail.com>
2024-01-14 13:29:06 -0500

    functions

---
+++ 02-Fundamentals-Part-2/33. Function/script.js
@@ -0,0 +1,23 @@
+// function declaration 
+
+function logger() {
+    console.log(`My name is Pavel`);
+}
+
+//function calling, running, onvoking
+logger();
+
+function fruitProcessor(apples, oranges) {
+    console.log(apples, oranges);
+    const juice = `Juice with ${apples} apples and ${oranges} oranges`;
+    return juice;
+}
+
+const appleJuice = fruitProcessor(5, 0);
+console.log(appleJuice);
+
+console.log(fruitProcessor(5, 0));
+
+
+const appleOrangeJuice = fruitProcessor(2, 4);
+console.log(appleOrangeJuice);
