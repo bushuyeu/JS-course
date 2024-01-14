0dace37e3c3da89647c1b7f1751e3d55dc00a7e6
Pavel Bushuyeu <pavelbushuyeu@gmail.com>
2024-01-14 10:30:08 -0500

    ternary operator

---
+++ 01-Fundamentals-Part-1/28. Ternary Operator/script.js
@@ -0,0 +1,12 @@
+const age = 23;
+
+//  
+
+// declaire variables conditionally
+const drink = age >= 18 ? 'wine' : 'water';
+console.log(drink);
+
+
+// conditional expressions inside of the ``:
+
+console.log(`I like to drink ${age >= 18 ? 'wine' : 'water'}`);
