5ba15cab5bbaa77f0200c4e729a6e9331d6433f9
Pavel Bushuyeu <pavelbushuyeu@gmail.com>
2024-01-20 15:37:02 -0500

    object

---
+++ 02-Fundamentals-Part-2/42. Objects/script.js
@@ -0,0 +1,10 @@
+// literal definition of the object 
+// order does not matter unlike in arrays 
+
+const pavel = {
+    firstName: 'Pavel';
+    lastName: 'Bushuyeu';
+    job: 'founder';
+    friends: ['Khafiz', 'Olga', 'Diana'];
+};
+
